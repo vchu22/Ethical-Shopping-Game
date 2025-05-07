a83a10e88375541087ec2bcbc2e24d821de667d2
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2025-05-07 15:14:06 -0400

    add Main menu UI

---
+++ Assets/Scripts/MainMenu_UI.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class MainMenu_UI : MonoBehaviour
+{
+    public void StartGame()
+    {
+        Helpers.NextScreen();
+    }
+    public void GotoOptionsPage()
+    {
+        
+    }
+}
