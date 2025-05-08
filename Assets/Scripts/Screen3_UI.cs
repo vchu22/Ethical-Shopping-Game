d5e31aca2b6a6e075dc7dce8ebd1456f03475b27
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2025-05-08 16:29:21 -0400

    feat: display summery based on numbers of positive and negative categories

--- Assets/Scripts/Screen3_UI.cs
+++ Assets/Scripts/Screen3_UI.cs
@@ -1,12 +1,19 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using static UnityEditor.Progress;
 
 public class Screen3_UI : MonoBehaviour
 {
     public Transform cartProductParent;
     public GameObject cartProductPrefab;
     public TextMeshProUGUI ethicInfoText;
+    public TextMeshProUGUI summeryInfoText;
+
+    public string summeryTextPositiveMore;
+    public string summeryTextNegativeMore;
+    public string summeryTextNeutral;
+
     public void Awake()
     {
         Debug.Log("Screen 3 Round " + GameState.currentRound);
@@ -19,17 +26,36 @@ public class Screen3_UI : MonoBehaviour
             GameObject cartProduct = Instantiate(cartProductPrefab, cartProductParent);
             cartProduct.GetComponent<CartProduct>().SetItem(item);
         }
+
+        // Display ethic info
+        int posCount = 0, negCount = 0;
         ethicInfoText.text = "";
         foreach (var item in GameState.shoppingCartItems)
         {
             foreach (string desc in item.positiveEthicCategories)
             {
                 ethicInfoText.text += ("+ " + desc + "\n");
+                posCount++;
             }
             foreach (string desc in item.negativeEthicCategories)
             {
                 ethicInfoText.text += ("- " + desc + "\n");
+                negCount++;
             }
+        }
+
+        // Show summery text based on how many positive and negative categories
+        if (posCount == negCount)
+        {
+            summeryInfoText.text = summeryTextNeutral;
+        }
+        else if (posCount > negCount)
+        {
+            summeryInfoText.text = summeryTextPositiveMore;
+        }
+        else 
+        {
+            summeryInfoText.text = summeryTextNegativeMore;
         }
     }
 
