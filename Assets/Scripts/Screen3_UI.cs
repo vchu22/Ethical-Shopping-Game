c0d19066f45b9cb7b7113408c6756ec8bdb16dcd
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2025-05-08 16:16:27 -0400

    feat: Screen 3 shows ethic info of all selected products

--- Assets/Scripts/Screen3_UI.cs
+++ Assets/Scripts/Screen3_UI.cs
@@ -4,10 +4,9 @@ using UnityEngine.UI;
 
 public class Screen3_UI : MonoBehaviour
 {
-    public Button nextRoundButton;
-    
     public Transform cartProductParent;
     public GameObject cartProductPrefab;
+    public TextMeshProUGUI ethicInfoText;
     public void Awake()
     {
         Debug.Log("Screen 3 Round " + GameState.currentRound);
@@ -19,6 +18,18 @@ public class Screen3_UI : MonoBehaviour
         {
             GameObject cartProduct = Instantiate(cartProductPrefab, cartProductParent);
             cartProduct.GetComponent<CartProduct>().SetItem(item);
+        }
+        ethicInfoText.text = "";
+        foreach (var item in GameState.shoppingCartItems)
+        {
+            foreach (string desc in item.positiveEthicCategories)
+            {
+                ethicInfoText.text += ("+ " + desc + "\n");
+            }
+            foreach (string desc in item.negativeEthicCategories)
+            {
+                ethicInfoText.text += ("- " + desc + "\n");
+            }
         }
     }
 
